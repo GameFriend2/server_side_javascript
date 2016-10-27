8503c8c05a2a4607ad8817a4ac41c98b65f5e58e
MaseKor <leynuxms@gmail.com>
2016-10-28 08:38:07 +0900

    12일차

--- app_mysql.js
+++ app_mysql.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var fs = require('fs');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var conn= mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
---
+++ app_template.js
@@ -0,0 +1,5 @@
+const express = require('express');
+var app = express();
+app.listen(3003, function(res,req){
+  console.log('Connedted 3003 port!!');
+})
