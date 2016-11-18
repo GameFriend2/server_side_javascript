aadae8510c427fd098ef55cc870af8e6d627ffad
TaiKi Kim <ktk6176@naver.com>
2016-11-18 21:49:42 +0900

    15일차
    수행 끝난 후 다시 시작
    session part2 진행중

--- app_session.js
+++ app_session.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var session = require('express-session');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended : false }));
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
@@ -13,10 +15,59 @@ app.get('/count', function(req, res){
         req.session.count = 1;
     }
     res.send('result : ' + req.session.count);
+});
+app.get('/welcome', function (req, res) {
+    if(req.session.displayName) {
+        res.send(`
+           <h1>Hello, ${req.session.displayName}</h1>
+           <a href="/auth/logout">logout</a>    
+        `);
+    }else{
+        res.send(`
+            <h2>Welcome</h2>
+            <a href="/auth/login">Login</a>
+        `)
+    }
+    //res.send(req.session);
 })
+app.post('/auth/login', function (req, res) {
+    var user = {
+        mid : 'txtk6176',
+        mpass : '1111',
+        displayName : 'Txtk6176'
+    };
+    var id = req.body.username;
+    var pass = req.body.password;
+    if(user.mid === id && user.mpass === pass){
+        req.session.displayName = user.displayName;
+        res.redirect('/welcome');
+    }
+    else res.send('login failed <a href="/auth/login">Login</a>');
+});
+app.get('/auth/login', function (req, res){
+    var output=`
+    <h1>Login</h1>
+    <form action="/auth/login" method="post">
+    <p>
+    <input type="text" name="username" placeholder="username">
+    </p>
+    <p>
+    <input type="password" name="password" placeholder="password"> 
+    </p>
+    <p>
+    <input type="submit" name="submit">
+</p>
+    </form>
+`;
+    res.send(output);
+});
 app.get('/tmp', function(req, res){
+
     res.send('result : ' + req.session.count);
 })
+app.get('', function (req, res) {
+    res.send()
+});
 app.listen(3000, function(){
     console.log('Connected 3000 port');
 })
